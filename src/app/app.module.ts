01a54023b9421959a2463603f4b70510665eef68
CodeSandbox <support@codesandbox.io>
2023-09-01 12:20:45 +0000

    Sandbox snapshot commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent } from "./app.component";
+import { GanttComponent } from "./gantt/gantt.component";
+
+@NgModule({
+  declarations: [AppComponent, GanttComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
